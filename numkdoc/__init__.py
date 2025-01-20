435dc7eac2c8580342c0c05ac776211cc2b98a2a
Thomas Fel <tfel@g.harvard.edu>
2025-01-20 18:56:31 +0100

    Bump version: 0.4.2 → 0.4.3

--- numkdoc/__init__.py
+++ numkdoc/__init__.py
@@ -1,3 +1,3 @@
 from .numkdoc import Numkdoc
 
-__version__ = '0.4.2'
+__version__ = '0.4.3'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 setup(
     name='Numkdoc',
     packages=['numkdoc'],
-    version='0.4.2',
+    version='0.4.3',
     license='MIT',
 
     description='Mkdoc plugin to autodoc your numpy docstring',
